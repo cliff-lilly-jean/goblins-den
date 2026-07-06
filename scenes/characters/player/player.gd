17cdbef8111b330ac3eb78dce5f8db91685adcae
Cliff Jean <cliff.lilly.jean@gmail.com>
2026-07-06 19:49:56 -0400

    added a run speed, walk speed, and player movement

--- scenes/characters/player/player.gd
+++ scenes/characters/player/player.gd
@@ -6,8 +6,14 @@ const MAX_ANGLE_LOOK_DOWN: float = deg_to_rad(-30)
 @export var mouse_sensitivity: float
 @export var joystick_sensitiity: float
 @export var camera_distance: float
+@export var walk_speed: float
+@export var run_speed: float
+@export var acceleration: float
+@export var jump_force: float
+@export var gravity: float
 
 @onready var spring_arm: SpringArm3D = %SpringArm3D
+@onready var input_dir : Vector2
 
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
@@ -24,12 +30,22 @@ func _input(event: InputEvent) -> void:
 
 func _process(delta: float) -> void:
 	
+	## Get the input direction that is being pressed
+	input_dir = Input.get_vector("strafe_left","strafe_right", "backward","forward").normalized()
+	
 	joystick_rotation(delta)
 
 
+func _physics_process(delta: float) -> void:
+	
+	move(delta)
+	jump()
+	process_gravity()
+	
+	move_and_slide()
+
 func joystick_rotation(delta: float) -> void:
 	
-	## Get the joystick input vector
 	var right_stick_vector = Input.get_vector("look_left","look_right","look_down","look_up").normalized()
 	
 	## Rotate the joystick around the y axis by a certain numbber and delta
@@ -38,3 +54,24 @@ func joystick_rotation(delta: float) -> void:
 	## Rotate the Spring Arm/Camera over the x axis and clamp the values so that they dont rotate around the player
 	spring_arm.rotation.x -= -right_stick_vector.y * joystick_sensitiity * delta
 	spring_arm.rotation.x = clampf(spring_arm.rotation.x, MAX_ANGLE_LOOK_DOWN, MAX_ANGLE_LOOK_UP)
+
+func move(delta: float) -> void:
+	var direction: Vector3 = Vector3(input_dir.x, 0, input_dir.y)
+	
+	var speed = run_speed if Input.is_action_pressed("run") else walk_speed
+	
+	var desired_velocity = transform.basis * direction * speed
+	
+	if direction == Vector3.ZERO:
+		velocity.x = move_toward(velocity.x, 0, acceleration * delta)	
+		velocity.z = move_toward(velocity.z, 0, acceleration * delta)	
+	velocity.x = move_toward(velocity.x, desired_velocity.x, acceleration * delta)
+	velocity.z = move_toward(velocity.z, desired_velocity.z, acceleration * delta)
+
+func jump():
+	if is_on_floor() and Input.is_action_just_pressed("jump"):
+		velocity.y = jump_force
+		
+func process_gravity() -> void: 
+	if not is_on_floor():
+		velocity.y -= gravity
